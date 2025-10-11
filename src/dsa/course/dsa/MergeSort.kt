8932dab2207e719e97579159981b89bf65c9fc21
techcodingacademy <techcodingacademy@gmail.com>
2025-10-11 15:21:08 -0400

    divide array and check each subarray

--- src/dsa/course/dsa/MergeSort.kt
+++ src/dsa/course/dsa/MergeSort.kt
@@ -1,20 +1,25 @@
 package dsa.course.dsa
 
 import java.util.Arrays
+// To perform merge sort, we need to follow a few steps.
+// First, we divide the array repeatedly until each subarray contains a single element.
+// Once we reach single elements, they are considered sorted.
+// Then, we proceed to merge these subarrays using the two-way merging process,
+// which is based on the principle of merging sorted arrays.
 
 fun main() {
     val newMergeSort = MergeSort()
-    val testedArray = arrayOf(13, 5, 9, 21, 2, 16)
+    val testedArray = arrayOf(13,5,9,21,2,16)
     newMergeSort.divideLogic(testedArray, low = 0, high = testedArray.size - 1)
     println(Arrays.toString(testedArray))
-    val checkThis = arrayOf(60,70,50,30,40)
-    println(checkThis.sliceArray(0..4).contentToString())
-    println(checkThis.sliceArray(0..1).contentToString())
-    println(checkThis.sliceArray(0..0).contentToString())
-    println(checkThis.sliceArray(1..1).contentToString())
-    println( checkThis.sliceArray(1..2).contentToString())
-    println( checkThis.sliceArray(2..3).contentToString())
-    println( checkThis.sliceArray(3..4).contentToString())
+    //val checkThis = arrayOf(13,5,9,21,2,16)
+    println(testedArray.sliceArray(0..4).contentToString())
+    println(testedArray.sliceArray(0..1).contentToString())
+    println(testedArray.sliceArray(0..0).contentToString())
+    println(testedArray.sliceArray(1..1).contentToString())
+    println( testedArray.sliceArray(1..2).contentToString())
+    println( testedArray.sliceArray(2..3).contentToString())
+    println( testedArray.sliceArray(3..4).contentToString())
 
 
 }
@@ -22,7 +27,8 @@ fun main() {
 class MergeSort {
     fun divideLogic(testedArray: Array<Int>, low: Int, high: Int) {
         if (low < high) {
-            val middle = (low + high) / 2
+            val middle = (low + high) / 2 //[13, 5, 9, 21, 2, 1]  ==> [13,5,9] and [21,2,1]
+
             println("Divide by ${testedArray.sliceArray(low..high).contentToString()}")
             println("Left division ${testedArray.sliceArray(low..middle).contentToString()}")
             println("Right division ${testedArray.sliceArray(middle + 1 .. high).contentToString()}")
@@ -30,5 +36,8 @@ class MergeSort {
             divideLogic(testedArray, low, middle) // 0 and 2
             divideLogic(testedArray, middle + 1, high)
         }
+    }
+    fun twoWaysMerge(){
+
     }
 }
