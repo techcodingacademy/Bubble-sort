fe8cca03bb1dafa2c46637c3cfa4ffa904ea32fc
techcodingacademy <techcodingacademy@gmail.com>
2025-10-13 16:48:43 -0400

    merge sort implementation

--- src/dsa/course/dsa/MergeSort.kt
+++ src/dsa/course/dsa/MergeSort.kt
@@ -1,6 +1,7 @@
 package dsa.course.dsa
 
 import java.util.Arrays
+
 // To perform merge sort, we need to follow a few steps.
 // First, we divide the array repeatedly until each subarray contains a single element.
 // Once we reach single elements, they are considered sorted.
@@ -9,7 +10,7 @@ import java.util.Arrays
 
 fun main() {
     val newMergeSort = MergeSort()
-    val testedArray = arrayOf(13,5,9,21,2,16)
+    val testedArray = arrayOf(13, 5, 9, 21, 2, 16)
     newMergeSort.divideLogic(testedArray, low = 0, high = testedArray.size - 1)
     println(Arrays.toString(testedArray))
     //val checkThis = arrayOf(13,5,9,21,2,16)
@@ -17,9 +18,9 @@ fun main() {
     println(testedArray.sliceArray(0..1).contentToString())
     println(testedArray.sliceArray(0..0).contentToString())
     println(testedArray.sliceArray(1..1).contentToString())
-    println( testedArray.sliceArray(1..2).contentToString())
-    println( testedArray.sliceArray(2..3).contentToString())
-    println( testedArray.sliceArray(3..4).contentToString())
+    println(testedArray.sliceArray(1..2).contentToString())
+    println(testedArray.sliceArray(2..3).contentToString())
+    println(testedArray.sliceArray(3..4).contentToString())
 
 
 }
@@ -28,16 +29,45 @@ class MergeSort {
     fun divideLogic(testedArray: Array<Int>, low: Int, high: Int) {
         if (low < high) {
             val middle = (low + high) / 2 //[13, 5, 9, 21, 2, 1]  ==> [13,5,9] and [21,2,1]
-
-            println("Divide by ${testedArray.sliceArray(low..high).contentToString()}")
-            println("Left division ${testedArray.sliceArray(low..middle).contentToString()}")
-            println("Right division ${testedArray.sliceArray(middle + 1 .. high).contentToString()}")
-            println("-----------------------------------------------------------------------------------")
             divideLogic(testedArray, low, middle) // 0 and 2
             divideLogic(testedArray, middle + 1, high)
+            merge(testedArray, low, middle, high)
         }
     }
-    fun twoWaysMerge(){
 
+    private fun merge(testedArray: Array<Int>, low: Int, middle: Int, high: Int) {
+        val leftArray = arrayOfNulls<Int>(middle - low + 1)
+        val rightArray = arrayOfNulls<Int>(high - middle)
+       // [13, 5, 9, 21, 2, 1]                    ====> [13,5,9] | [21,2,1
+        for (i in leftArray.indices){
+            leftArray[i] = testedArray[i + low]
+        }
+
+        for (j in rightArray.indices){
+           rightArray[j] = rightArray[middle + 1 + j]
+        }
+
+        var (i, j, k) = listOf(0, 0, 0)
+        while (i < leftArray.size && j < rightArray.size) {
+            if (leftArray[i]!! < rightArray[j]!!) {
+                testedArray[k] = leftArray[i]!!
+                i++
+            } else {
+                testedArray[k] = leftArray[j]!!
+                j++
+            }
+            k++
+        }
+        while (i < leftArray.size) {
+            testedArray[k] = leftArray[i]!!
+            i++
+            k++
+        }
+        while (j < rightArray.size) {
+            testedArray[k] = leftArray[j]!!
+            j++
+            k++
+        }
     }
+
 }
