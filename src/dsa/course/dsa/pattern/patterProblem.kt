9ec4295d8f4ea3755fc6338c978b473cd6e0da92
Ola <techcodingacademy@gmail.com>
2025-12-05 00:40:15 -0500

    pattern printing logic added triangle

--- src/dsa/course/dsa/pattern/patterProblem.kt
+++ src/dsa/course/dsa/pattern/patterProblem.kt
@@ -7,6 +7,8 @@ fun main(){
     println( pyramid())
     //println(levelUp())
     println(diamondPattern(5))
+    invertedTriangle(5)
+    newTriangle(5)
 }
 
 fun squarePattern(numbers: Int){
@@ -90,13 +92,41 @@ fun diamondPattern(n:Int){
     }
 
 }
+//
+//You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order, and each of their nodes contains a single digit. Add the two numbers and return the sum as a linked list.
+//
+//You may assume the two numbers do not contain any leading zeros, except the number 0 itself.
+
+//Input: Two linked lists of integers
+//out: sum as a linked list
+// own word: I am giving two non-negative or empty linked lists, the digits are stored in reverse order,
+// I need to iterate over the two lists, each number, adding each number together.Finally, return sum as a linkedlist
+
+//0,1,2,3,4
+//
+
+fun invertedTriangle(n: Int){
+    for (i in 0 until n){
+
+        for (j in 0 until i + 1){
+            print(j+1)
+        }
+        println()
+    }
+}
 
 
-
-
-
-
-
+fun newTriangle(n: Int){
+    for (i in 0 until n){ // n = 5
+        for (space in 0 until (n-1)-i){
+            print(" ")
+        }
+        for (j in 0 until (2 * i) + 1){
+            print("*")
+        }
+        println()
+    }
+}
 
 
 
